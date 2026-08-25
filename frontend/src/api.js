726faa5e7bbca12213187003610ba49d2796e7cd
Ashish-T <54099378+Ashish-T@users.noreply.github.com>
2026-08-25 19:33:37 +0530

    Delete frontend/src directory

--- frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-// This points to your live backend on Render
-const API = axios.create({
-    baseURL: 'https://tms-app-c03d.onrender.com',
-});
-
-export default API;
