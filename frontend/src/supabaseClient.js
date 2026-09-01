3f0a0f610c2ad4d87140067521b3ab00e9bcf429
Ashish-T <54099378+Ashish-T@users.noreply.github.com>
2026-09-01 14:07:16 +0000

    fix scroll isue

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Ensure the URL starts with https:// and has no brackets
-const supabaseUrl = 'https://hsirbelsywximwapqktq.supabase.co'; 
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; 
+const supabaseUrl = 'https://dadfvklxxuvdqyiobxfe.supabase.co'; 
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhZGZ2a2x4eHV2ZHF5aW9ieGZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgyNDcwODgsImV4cCI6MjEwMzgyMzA4OH0.nmegf21NphvQKd8ci1YRCVwW9N4sa1qW5pUc-5IDuvM'; 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
