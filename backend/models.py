faf2b19dd91b4eb820a861508a53255d8e213641
Ashish-T <54099378+Ashish-T@users.noreply.github.com>
2026-09-01 20:01:56 +0000

    new version

--- backend/models.py
+++ backend/models.py
@@ -29,7 +29,7 @@ class User(Base):
     supervisor_id = Column(Integer, ForeignKey('users.id'), nullable=True)
 
 class TripLog(Base):
-    __tablename__ = 'trip_logs_v12' 
+    __tablename__ = 'trip_logs_v11' 
     id = Column(Integer, primary_key=True)
     driver_id = Column(Integer, ForeignKey('users.id'))
     supervisor_id = Column(Integer, ForeignKey('users.id'))
